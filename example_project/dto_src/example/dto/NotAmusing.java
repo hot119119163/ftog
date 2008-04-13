3025dbabc8bb995ac32de51599601dda77c6f4de
mattias.anstrand <mattias.anstrand@5df45002-5f46-0410-8544-27eea78f1d61>
2008-04-13 16:55:19 +0000

    Class ignore list example

---
+++ example_project/dto_src/example/dto/NotAmusing.java
@@ -0,0 +1,14 @@
+package example.dto;
+
+public class NotAmusing {
+	//TODO: This class need some work before it could be used.
+	public static final int BOORING_VALUE=12;
+	
+	public int getUselessValue() {
+		return 13;
+	}
+	
+	public void setUselessValue(int value) {
+		
+	}
+}
