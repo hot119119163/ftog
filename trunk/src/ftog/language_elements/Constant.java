090496a0e8c382b100bb1e8f9163975263e1688f
mattias.anstrand@gmail.com <mattias.anstrand@gmail.com@5df45002-5f46-0410-8544-27eea78f1d61>
2015-01-23 13:25:35 +0000

    Changed static constant representation in Javascript

--- trunk/src/ftog/language_elements/Constant.java
+++ trunk/src/ftog/language_elements/Constant.java
@@ -4,14 +4,16 @@ import ftog.main.FlexCodeFormatting;
 
 public class Constant extends Property {
 	public Object value;
-	
+	public String motherClass;
+
 	public String toFlexCode() {
 		StringBuffer sb = new StringBuffer();
 		sb.append((isPrivate ? "private " : "public ") +"static const "+name+":"+flexClass+" = "+value+";");		
 		return sb.toString();
 	}
-	
+
+	@Override
 	public String toJavascriptCode() {
-		return "this.prototype."+name+"="+value+";";
+		return name+"="+value+";";
 	}
 }
